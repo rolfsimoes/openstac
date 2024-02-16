2b35ee34b9ec593fa362cf7b6ac07c8492520150
Rolf Simoes <rolf.simoes@opengeohub.org>
2024-02-16 12:29:25 +0100

    Add package description

---
+++ R/openstac-package.R
@@ -0,0 +1,17 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' openstac
+#'
+#' @section Purpose:
+#'
+#' The package provides a comprehensive set of functions for building
+#' Spatio-Temporal Asset Catalog (STAC) API implementations. With
+#' `openstac`, users can easily deploy geospatial data into a STAC API
+#' service enhancing interoperability with other geotechnologies.
+#'
+#' @name openstac-package
+#' @aliases openstac
+## usethis namespace: end
+NULL
